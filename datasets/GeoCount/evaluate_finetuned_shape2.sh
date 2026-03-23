4ed7094fc086fbd474a509dddbe620cdc7b58d91
Haochen Zhang <hz112@rice.edu>
2026-03-23 15:28:04 -0500

    0323

--- datasets/GeoCount/evaluate_finetuned_shape2.sh
+++ datasets/GeoCount/evaluate_finetuned_shape2.sh
@@ -1,7 +1,8 @@
-CUDA_VISIBLE_DEVICES=2 python evaluate_finetuned.py \
+CUDA_VISIBLE_DEVICES=4 python evaluate_finetuned.py \
   --task_json ./shapes_2/test_task.json \
   --model_name_or_path /playpen-shared/haochenz/DeepSeek-R1-Distill-Qwen-7B \
-  --output_path outputs/deepseek_r1_distill_qwen_7b_shapes2.jsonl \
+  --adapter_path /playpen-shared/haochenz/MultiModalRL/grpo_ckpt/shapes_2/checkpoint-300 \
+  --output_path outputs/finetuned_deepseek_r1_distill_qwen_7b_shapes2.jsonl \
   --use_chat_template \
   --dtype bfloat16 \
   --max_new_tokens 16384
