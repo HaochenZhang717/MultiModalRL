189d00ec706b7cec03712ff479750da67b567830
Haochen Zhang <hz112@rice.edu>
2026-04-04 13:28:39 -0500

    0404

--- code/train_grpo.sh
+++ code/train_grpo.sh
@@ -1,4 +1,8 @@
 export CUDA_VISIBLE_DEVICES=0,2,3,4
+export HF_HOME=/playpen-shared/haochenz/hf_cache
+export TRANSFORMERS_CACHE=$HF_HOME
+export HF_DATASETS_CACHE=$HF_HOME
+
 torchrun \
   --nproc_per_node=4 \
   code/train_grpo.py
