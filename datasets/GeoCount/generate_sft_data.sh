5e6b3d1642f96b92a1b5d5b17d85bbe2dc2e6d27
Haochen Zhang <hz112@rice.edu>
2026-04-04 12:13:03 -0500

    0404

--- datasets/GeoCount/generate_sft_data.sh
+++ datasets/GeoCount/generate_sft_data.sh
@@ -3,4 +3,4 @@ export TRANSFORMERS_CACHE=$HF_HOME
 export HF_DATASETS_CACHE=$HF_HOME
 
 
-CUDA_VISIBLE_DEVICES=4  python datasets/GeoCount/generate_sft_thinking.py --batch_size 16
+CUDA_VISIBLE_DEVICES=4  python generate_sft_thinking.py --batch_size 16
